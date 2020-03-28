709276d154e9aa9420e410278d0d54cf4c11f323
Ananth Choalla <ananth@venkatas-mbp.lan>
2020-03-28 14:03:51 -0700

    Inital springboot stub

---
+++ src/main/java/org/cellarium/CellariumApplication.java
@@ -0,0 +1,13 @@
+package org.cellarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellariumApplication.class, args);
+	}
+
+}
